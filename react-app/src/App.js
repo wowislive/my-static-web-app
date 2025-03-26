48c96a6ed8411eff2aca5bf417f1e1cffd20db48
Dmitrii Pavlov <wowislive@gmail.com>
2025-03-26 19:05:52 +0200

    api test with function

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,36 +1,17 @@
-import React, { Component, lazy, Suspense } from 'react';
-import 'bulma/css/bulma.css';
-import './styles.scss';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { withRouter } from 'react-router';
-import { HeaderBar, NavBar, NotFound } from './components';
-import About from './About';
+import React, { useState, useEffect } from 'react';
 
-const Products = withRouter(
-  lazy(() => import(/* webpackChunkName: "products" */ './products/Products'))
-);
+function App() {
+  const [data, setData] = useState('');
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <HeaderBar />
-        <div className="section columns">
-          <NavBar />
-          <main className="column">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Switch>
-                <Redirect from="/" exact to="/products" />
-                <Route path="/products" component={Products} />
-                <Route path="/about" component={About} />
-                <Route exact path="**" component={NotFound} />
-              </Switch>
-            </Suspense>
-          </main>
-        </div>
-      </div>
-    );
-  }
+  useEffect(() => {
+    (async function () {
+      const response = await fetch(`/api/message`);
+      const text = await response.text();
+      setData(text);
+    })();
+  }, []);
+
+  return <div>{data}</div>;
 }
 
 export default App;
